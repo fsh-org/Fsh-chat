943cc161d0b7d21473ac125feb4cb75c15788503
inventionpro <109528211+inventionpro@users.noreply.github.com>
2024-03-23 15:53:39 +0000

    Make codefactor happy

--- public/data.js
+++ public/data.js
@@ -1,11 +1,11 @@
 // Name save
-document.getElementById("name").addEventListener("keyup", (event) => {
+document.getElementById("name").addEventListener("keyup", () => {
   localStorage.setItem("name", document.getElementById("name").value);
 });
 document.getElementById("name").value = localStorage.getItem("name");
 
 // Color save
-document.getElementById("color").addEventListener("input", (event) => {
+document.getElementById("color").addEventListener("input", () => {
   localStorage.setItem("color", document.getElementById("color").value);
 });
 document.getElementById("color").value = localStorage.getItem("color") || '#8888ff';
