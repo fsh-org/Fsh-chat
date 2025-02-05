054f005362e3e1961f718c44bee15324a6936a31
inventionpro <109528211+inventionpro@users.noreply.github.com>
2025-02-05 17:24:39 +0100

    Notice

--- public/data.js
+++ public/data.js
@@ -8,4 +8,8 @@ document.getElementById("name").value = localStorage.getItem("name");
 document.getElementById("color").addEventListener("input", () => {
   localStorage.setItem("color", document.getElementById("color").value);
 });
-document.getElementById("color").value = localStorage.getItem("color") || '#8888ff';
+document.getElementById("color").value = localStorage.getItem("color") ?? '#8888ff';
+
+function disclose() {
+  document.getElementById('disclose').showModal();
+}
