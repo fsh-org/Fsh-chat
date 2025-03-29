09f5f9095c2be6e4367892acd23af7f3b380510a
inventionpro <109528211+inventionpro@users.noreply.github.com>
2025-03-29 15:23:50 +0100

    File fix & cleanup

--- public/files.js
+++ public/files.js
@@ -1,36 +1,42 @@
-function DataToElem(data) {
-  return `<${data.includes("image/") ? "img" : data.includes("video/") ? "video controls" : data.includes("audio/") ? "audio controls" : "img"} src="${data}" class="FOPImg">${data.includes("image/") ? "" : data.includes("video/") ? "</video>" : data.includes("audio/") ? "</audio>" : ""}`
+// Data
+const typeOpen = {
+  image: 'img',
+  video: 'video',
+  audio: 'audio'
+}
+const typeClose = {
+  image: '',
+  video: '</video>',
+  audio: '</audio>'
+}
+const typeIcons = {
+  image: 'image',
+  video: 'video',
+  audio: 'audio'
 }
 
+// Utility functions
+function DataToElem(data) {
+  if (data.startsWith('https://')) return `<img src="${data}" class="FOPImg">`;
+  return `<${typeOpen[data.replace('data:','').split('/')[0]]??'div'} src="${data}" class="FOPImg" controls>${typeClose[data.replace('data:','').split('/')[0]]??'</div>'}`;
+}
 function ico(e) {
-  let types = {
-    image: 'image',
-    video: 'video',
-    audio: 'audio'
-  }
+  if (e.includes("tenor.com")) return `<img width="50px" height="14px" src="/images/tenor.svg">`;
 
-  if (e.includes("tenor.com")) {
-    return `<img width="50px" height="14px" src="/images/tenor.svg">`
-  }
-
-  if (String(types[e.split("/")[0].replace("data:",'')]) != 'undefined') {
-    return `<i class="fa-solid fa-file-${types[e.split("/")[0].replace("data:",'')]}"></i>`
+  if (String(typeIcons[e.split("/")[0].replace("data:",'')]) != 'undefined') {
+    return `<i class="fa-solid fa-file-${typeIcons[e.split("/")[0].replace("data:",'')]}"></i>`;
   } else {
-    return `<i class="fa-solid fa-file-circle-question"></i>`
+    return `<i class="fa-solid fa-file-circle-question"></i>`;
   }
 }
 
 // Update File Preview
 function UFP() {
-  let fil = "";
-  AtachementFiles.forEach(e=>{
-    fil += `<div class="FileOnPreview">
-<button class="FOPDelete" onclick="AtachementFiles[AtachementFiles.indexOf(this.parentElement.children[1].src)]='';this.parentElement.remove()"><i class="fa-solid fa-trash" style="color: #ff0000;"></i></button>
-<div class="FOPIcon">${ico(e)}</div>
-${DataToElem(e)}
-</div>`
-  })
-  document.getElementById("preview").innerHTML = fil;
+  document.getElementById("preview").innerHTML = AtachementFiles.map(file=>`<div class="FileOnPreview">
+<button class="FOPDelete" onclick="AtachementFiles[AtachementFiles.indexOf(this.parentElement.children[2].src)]='';this.parentElement.remove()"><i class="fa-solid fa-trash" style="color:#ff0000"></i></button>
+<div class="FOPIcon">${ico(file)}</div>
+${DataToElem(file)}
+</div>`).join('');
 }
 
 function resizeBase64Img(base64, newWidth, newHeight) {
@@ -42,9 +48,9 @@ return new Promise((resolve, reject)=>{
   let img = document.createElement("img");
   img.src = base64;
   img.onload = function () {
-    context.scale(newWidth/img.width,  newHeight/img.height);
-    context.drawImage(img, 0, 0); 
-    resolve(canvas.toDataURL());               
+    context.scale(newWidth/img.width, newHeight/img.height);
+    context.drawImage(img, 0, 0);
+    resolve(canvas.toDataURL());
   }
 });
 }
@@ -52,38 +58,39 @@ function sizeBase64Img(base64) {
   return new Promise((resolve, reject)=>{
     let img = document.createElement("img");
     img.src = base64;
-    img.onload = function () {
-      resolve([img.width, img.height]);               
+    img.onload = function(){
+      resolve([img.width, img.height]);
     }
   });
 }
 
 let AtachementFiles = [];
 
-var fel = document.getElementById('files')
-fel.onchange = function() {
-  Array.from(fel.files).forEach(e => {
+// Adding files
+let fel = document.getElementById('files')
+fel.onchange = function(){
+  Array.from(fel.files).forEach(file=>{
+    // Read file
     let dat = new FileReader();
-    dat.readAsDataURL(e);
+    dat.readAsDataURL(file);
     dat.addEventListener("loadend", async() => {
       let final = dat.result;
-      if (e.type.includes('image/')) {
+      if ((file.type.includes('image/') && file.type.includes("gif")) || file.type.includes("video/") || file.type.includes("audio/")) {
+        // Normal, ignore
+      } else if (file.type.includes('image/')) {
+        // Images, try to compress
         let size = await sizeBase64Img(dat.result)
         let compress = await resizeBase64Img(dat.result, size[0]/2, size[1]/2)
-        final = compress
-        if (dat.result.length < final.length) {
-          final = dat.result
+        // Is size smaller?
+        if (dat.result.length > compress.length) {
+          final = compress;
         }
-        if (e.type.includes("gif")) {
-          final = dat.result
-        }
-      } else if (e.type.includes("video/") || e.type.includes("audio/")) {
-        final = dat.result
       } else {
-        alert('Unsuported file type ' + e.type)
+        alert(`Unsuported file type ${file.type}`);
       }
       AtachementFiles.push(final);
       UFP();
     })
-  })
+  });
+  fel.value = '';
 }
